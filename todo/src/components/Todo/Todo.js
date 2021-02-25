5b61ea07d9ae25e70d0e857ac8475987147883cf
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-02-25 11:05:40 +0200

    chenged Todo to class

--- todo/src/components/Todo/Todo.js
+++ todo/src/components/Todo/Todo.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Todo.css";
-const Todo = ({ title, author, priority, id, status, removeFromList, editStatusTask }) => {
-  return (
-    <div className="Todo">
-      <div onClick={()=>editStatusTask(id)}>
-        <p className={`Todo__name ${status && "done"}`}>{title}</p>
-        <p className={`Todo__priority ${status && "done"}`}> {priority}</p>
-        <p className={`Todo__author ${status && "done"}  `}>{author}</p>
+
+class Todo extends Component {
+  editItem = () => {
+    this.props.editStatusTask(this.props.id);
+  };
+
+  removeItem = () => {
+    this.props.removeFromList(this.props.id);
+  };
+
+  render() {
+    const { title, author, priority, status } = this.props;
+    return (
+      <div className="Todo">
+        <div onClick={this.editItem}>
+          <p className={`Todo__name ${status && "done"}`}>{title}</p>
+          <p className={`Todo__priority ${status && "done"}`}> {priority}</p>
+          <p className={`Todo__author ${status && "done"}  `}>{author}</p>
+        </div>
+        <span onClick={this.removeItem} className="Todo__remove">
+          X
+        </span>
       </div>
-      <span onClick={() => removeFromList(id)} className="Todo__remove">
-        X
-      </span>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Todo;
 
