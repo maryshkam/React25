8c4f1ac5a86eed4370f55b0b44a124ec8eae581a
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-02-26 10:09:28 +0200

    added todo

--- todo/src/components/Todo/Todo.js
+++ todo/src/components/Todo/Todo.js
@@ -2,6 +2,48 @@ import React, { Component } from "react";
 import "./Todo.css";
 
 class Todo extends Component {
+  // state = {
+  //   number: 0
+  // }
+
+  // UNSAFE_componentWillMount(){
+  //   console.log('hi');
+  // }
+
+  componentDidMount() {
+    console.log("Component Did Mount");
+  }
+
+  componentWillReceiveProps() {
+    console.log("receive props");
+  }
+  shouldComponentUpdate() {
+    console.log("should component Update");
+    return true;
+  }
+
+  componentWillMount() {
+    // console.log(this.state.number);
+    console.log("willMount");
+    // this.setState({number:5})
+  }
+
+  componentWillUpdate() {
+    console.log("componentWillUpdate");
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    
+  }
+  
+
+  getSnapshotBeforeUpdate(){
+    console.log('snapshot');
+  }
+
+  componentDidUpdate(){
+    console.log('did update');
+  }
   editItem = () => {
     this.props.editStatusTask(this.props.id);
   };
@@ -11,6 +53,7 @@ class Todo extends Component {
   };
 
   render() {
+    console.log("render my component");
     const { title, author, priority, status } = this.props;
     return (
       <div className="Todo">
@@ -18,6 +61,7 @@ class Todo extends Component {
           <p className={`Todo__name ${status && "done"}`}>{title}</p>
           <p className={`Todo__priority ${status && "done"}`}> {priority}</p>
           <p className={`Todo__author ${status && "done"}  `}>{author}</p>
+          {/* <p>{this.state.number}</p> */}
         </div>
         <span onClick={this.removeItem} className="Todo__remove">
           X
