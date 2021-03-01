c7332fe1ff777fd10564fe56c6d43feb19dcd1bf
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-03-01 13:09:53 +0200

    added request in form

--- rest/src/App.js
+++ rest/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   state = {
     repos: [],
     users: [],
-    loader: true,
+    loader: false,
     error: false,
     text: "",
   };
@@ -36,33 +36,90 @@ class App extends Component {
   //   });
   // }
 
-  componentDidMount() {
+  async componentDidMount() {
     // const url =
     //   "https://api.github.com/search/repositories?q=react&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}";
     const url = withCreadentials(
       "https://api.github.com/search/repositories?q=react&"
     );
-    request("get", url)
-      .then((data) => {
-        this.setState({
-          repos: [...data.items],
-          loader: false,
-        });
-      })
-      .catch((error) => {
-        this.setState({
-          error: true,
-          loader: false,
-          text: error.message,
-        });
+
+    try {
+      this.toogleLoader();
+      const result = await request("get", url);
+      this.setState({
+        repos: [...result.items],
       });
+    } catch (error) {
+      this.toogleError();
+      this.setState({
+        text: error.message,
+      });
+    } finally {
+      this.toogleLoader();
+    }
+    //   request("get", url)
+    //     .then((data) => {
+    //       this.toogleLoader();
+    //       this.setState({
+    //         repos: [...data.items],
+    //       });
+    //     })
+    //     .catch((error) => {
+    //       this.toogleLoader();
+    //       this.toogleError();
+    //       this.setState({
+    //         text: error.message,
+    //       });
+    //     });
+    // }
+
+    // componentDidUpdate(){
+    //   const {search}= this.state;
+    //   const url = withCreadentials(
+    //     `https://api.github.com/search/repositories?q=${search}&`
+    //   );
+    //   request("get", url)
+    //     .then((data) => {
+    //       this.setState({
+    //         repos: [...data.items],
+    //         loader: false,
+    //       });
+    //     })
+    //     .catch((error) => {
+    //       this.setState({
+    //         error: true,
+    //         loader: false,
+    //         text: error.message,
+    //       });
+    //     });
   }
+
+  getSearch = (data) => {
+    this.setState({
+      repos: [...data],
+    });
+  };
+
+  toogleLoader = () => {
+    this.setState({
+      loader: !this.state.loader,
+    });
+  };
+  toogleError = () => {
+    this.setState({
+      error: !this.state.error,
+    });
+  };
 
   render() {
     const { repos, loader, error } = this.state;
     return (
       <div className="wrapper">
-        <Form />
+        <Form
+          getSearch={this.getSearch}
+          toogleLoader={this.toogleLoader}
+          toogleError={this.toogleError}
+        />
         <List repos={repos} loader={loader} error={error} />
       </div>
     );
--- rest/src/Components/Form/Form/Form.js
+++ rest/src/Components/Form/Form/Form.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import "./Form.css"
+import React, { Component } from "react";
+import { withCreadentials, request } from "../../../helpers/request";
 
-const Form = () => {
+import "./Form.css";
 
+class Form extends Component {
+  state = {
+    search: "",
+  };
 
+  handleInput = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      search: value,
+    });
+  };
 
-  return (
-    <form className="search-form">
-      <input type="text"/>
-      <button type="submit">Search</button>
-    </form>
-  );
-};
+  submitHeandler = async (e) => {
+    e.preventDefault();
+
+    const url = withCreadentials(
+      `https://api.github.com/search/repositories?q=${this.state.search}&`
+    );
+
+    try {
+      this.props.toogleLoader();
+      const result = await request("get", url);
+      await this.props.getSearch(result.items);
+    } catch (error) {
+      this.props.toogleError()
+    } finally {
+      this.props.toogleLoader();
+      this.setState({
+        search: "",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.submitHeandler} className="search-form">
+        <input
+          onChange={this.handleInput}
+          type="text"
+          value={this.state.search}
+        />
+        <button type="submit">Search</button>
+      </form>
+    );
+  }
+}
 
 export default Form;
