b01a4d986a46b42720f7046e27b5948dfa85d249
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-03-01 10:35:08 +0200

    added test for length

--- rest/src/Components/Form/List/List.js
+++ rest/src/Components/Form/List/List.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ListItem from "../ListItem/ListItem";
-import "./List.css"
-
+import "./List.css";
 
 const List = ({ repos }) => {
   return (
-    <ul className='list'>
-      {repos.map((repo) => (
-        <ListItem key={repo.id} {...repo} />
-      ))}
+    <ul className="list">
+      {repos.length > 0 ? (
+        repos.map((repo) => <ListItem key={repo.id} {...repo} />)
+      ) : (
+        <h1>Empty Page and loading data</h1>
+      )}
     </ul>
   );
 };
