2fe9dd23e0f90ba77439798c7add7171698f9b58
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-03-01 10:24:41 +0200

    added styles

--- rest/src/Components/Form/List/List.js
+++ rest/src/Components/Form/List/List.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ListItem from "../ListItem/ListItem";
+import "./List.css"
+
 
 const List = ({ repos }) => {
   return (
-    <ul>
+    <ul className='list'>
       {repos.map((repo) => (
         <ListItem key={repo.id} {...repo} />
       ))}
--- rest/src/Components/Form/ListItem/ListItem.js
+++ rest/src/Components/Form/ListItem/ListItem.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
+import "./ListItem.css";
 
-const ListItem = ({full_name}) => {
+const ListItem = ({ full_name, owner }) => {
+  const { avatar_url, login, type } = owner;
   return (
-    <li>
-      <h2>{full_name}</h2>
-
+    <li className="card">
+      <img className="card-img" src={avatar_url} alt=""></img>
+      <h2 className="card-title">{full_name}</h2>
+      <p className="card-text">{login}</p>
+      <p className="card-text">{type}</p>
     </li>
   );
 };
